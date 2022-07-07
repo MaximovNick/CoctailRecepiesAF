d182009c68f2f0c19afdcfc4df8f45de2dca409d
Nikolai Maximov <kolkamaximov@gmail.com>
2022-07-07 23:27:55 +0300

    CustomUI

---
+++ CoctailRecepiesAF/CustomUI/MargaritaViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MargaritaViewCell.swift
+//  CoctailRecepiesAF
+//
+//  Created by Nikolai Maksimov on 06.07.2022.
+//
+
+import UIKit
+
+class MargaritaViewCell: UITableViewCell {
+    
+    @IBOutlet var margaritasImage: UIImageView!
+    
+    @IBOutlet var margaritasNameLabel: UILabel!
+    
+    @IBOutlet var margaritasComponentLabel: UILabel!
+    
+}
